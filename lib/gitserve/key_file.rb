2e0ae5aa779541f27579cf56eac371ceeef7f5f4
Robert <hamstar@telescum.co.nz>
2012-11-18 01:36:54 +1300

    fixed tabs

--- lib/gitserve/key_file.rb
+++ lib/gitserve/key_file.rb
@@ -4,29 +4,29 @@ module GitServe
 
   module KeyFile
     def KeyFile.add(key, user, repo)
-	  key_line = "command=\"/usr/bin/gitserve #{user} #{repo} rw\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty #{key}"
-	  delete_key key if find_key key
+      key_line = "command=\"/usr/bin/gitserve #{user} #{repo} rw\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty #{key}"
+      delete_key key if find_key key
       File.open( AUTHORIZED_KEYS, 'a') {|f|
-  		f.puts key_line
-	  }
+        f.puts key_line
+      }
     end
   
     def KeyFile.find( key )
       File.readlines( AUTHORIZED_KEYS ).each do |line|
-	    return line if line.include? key
-	  end
+        return line if line.include? key
+      end
 	
-	  false
+      false
     end
   
     def KeyFile.delete( key )
       new_contents = File.read( AUTHORIZED_KEYS ).lines.reject { |line|
-	    line.include? key
-	  }.join "\n"
+        line.include? key
+      }.join "\n"
 	
-   	  File.open( AUTHORIZED_KEYS, "w" ) {|f|
-	    f.write new_contents
-  	  }
+      File.open( AUTHORIZED_KEYS, "w" ) {|f|
+        f.write new_contents
+      }
     end
   end
 end
