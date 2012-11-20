6a5505724c499bb611ba8a3f51fb34404f537728
Robert McLeod <hamstar@telescum.co.nz>
2012-11-20 03:56:24 +0000

    added file locations

--- lib/gitserve.rb
+++ lib/gitserve.rb
@@ -1,9 +1,19 @@
 module GitServe
 
+  class InvalidRepositoryError < StandardError; end
+
   class << self
 
     def authorized_keys
       File.expand_path("~/.ssh/authorized_keys")
+    end
+
+    def user_list
+      File.expand_path("~/.gitserve/users")
+    end
+
+    def repo_list
+      File.expand_path("~/.gitserve/repos")
     end
   end
 end
